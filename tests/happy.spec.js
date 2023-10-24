54136bba7c5f729bafdaa8b6f6f208b5a4812a36
HuijaeLee <Huijae.lee@cndfactory.com>
2023-10-25 02:19:38 +0900

    신규 품목생성 케이스 작성 중..

--- tests/happy.spec.js
+++ tests/happy.spec.js
@@ -1,17 +1,21 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-/**
- * Common
- */
-const pageUrl = 'http://happytest.spc.co.kr/' // Dev
-// const pageUrl = 'https://happy.spc.co.kr/'    // Prod
+
+/** Common *********************/
+const pageUrl = 'http://happytest.spc.co.kr/'   // Dev
+// const pageUrl = 'https://happy.spc.co.kr/'      // Prod
+
+var loginId = 'sg639'
+var loginPw = 'sys1234'
 
 
+/** Test Case *********************/
+
 /**
- * 페이지 접속
+ * 1. 페이지 접속
  */
-test('페이지 접속', async ({ page }) => {
+test('1. 페이지 접속', async ({ page }) => {
   await page.goto(pageUrl);
 
   // 최초 페이지 진입 여부 확인
@@ -20,29 +24,38 @@ test('페이지 접속', async ({ page }) => {
 
 
 /**
- * 2. 로그인
+ * 2. 로그인 / 로그아웃
  */
-test('로그인', async ({ page }) => {
+test('2. 로그인 / 로그아웃', async ({ page }) => {
 
   // 페이지 접속
   await page.goto(pageUrl);
-  const content = page.frameLocator('frame[name="content"]');
+
+  // 로그인 페이지 정보
+  const loginPage = page.frameLocator('frame[name="content"]');
 
   // 로그인 ID/PW 입력
-  await content
-      .locator('[name="P_USER_ID"]')
-      .fill('cndfactory');
-  await content
-      .locator('[name="P_USER_PW_TEMP"]')
-      .fill('sys1234');
+  await loginPage.locator('[name="P_USER_ID"]')
+      .fill(loginId);
+  await loginPage.locator('[name="P_USER_PW_TEMP"]')
+      .fill(loginPw);
 
   // 로그인 버튼 클릭
-  await content
-      .locator('#login_box06')
+  await loginPage.locator('#login_box06')
       .click();
 
-  // 로그인 성공 및 페이지 진입 확인
-  await expect(page).toHaveTitle(/해피구매시스템/);
+  // 로그인 및 페이지 진입 확인 (6초이내)
+  await expect(page).toHaveURL(/main/, { timeout: 6000 });
+  console.log('1. 로그인 성공');
+
+  // 로그아웃 버튼 클릭
+  await page.locator('.todolist_btn > a')
+      .first()
+      .click();
+
+  // 로그아웃 확인
+  await expect(page).not.toHaveURL(/main/);
+  console.log('2. 로그아웃 성공');
 
 });
 
@@ -50,6 +63,28 @@ test('로그인', async ({ page }) => {
 /**
  * 3. 신규 품목생성
  */
-test ('신규 품목생성', async ({ page }) => {
+test ('3. 신규 품목생성', async ({ page }) => {
+
+  // 페이지 접속 및 로그인 확인
+  await page.goto(pageUrl);
+  const content = page.frameLocator('frame[name="content"]');
+  await content.locator('[name="P_USER_ID"]').fill(loginId);
+  await content.locator('[name="P_USER_PW_TEMP"]').fill(loginPw);
+  await content.locator('#login_box06').click();
+  await expect(page).toHaveURL(/main/, { timeout: 6000 });
+
+  // 메뉴 진입 (기준정보 > 품목관리 > )
+  await page.getByRole('link', { name: '기준정보' })
+      .first()
+      .click();
+
+  await page.getByRole('link', { name: '품목관리' })
+      .click();
+
+  await page.getByRole('link', { name: '품목관리' })
+      .click();
+
+  await page.getByRole('link', { name: '- 품목 생성요청' })
+      .click();
 
 });
